94ef9779fbc8d4ca4b8334d2284947e28673e077
orioldedios <orioldedios@gmail.com>
2024-06-27 15:04:05 +0200

    now bodies spawn randomly

--- sharkooks/Assets/Scripts/Spawner.cs
+++ sharkooks/Assets/Scripts/Spawner.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     private float timer = 0.0f;
-    
+    private GameObject body_iterator = null;
+    private int random_iterator = 1;
+
     public float spawn_time = 5.0f;
-    public GameObject body_to_spawn = null;
+    public GameObject body_to_spawn_1 = null;
+    public GameObject body_to_spawn_2 = null;
+    public GameObject body_to_spawn_3 = null;
 
     // Start is called before the first frame update
     void Start()
@@ -21,8 +25,23 @@ public class Spawner : MonoBehaviour
         if (timer < spawn_time) timer += Time.deltaTime;
         else
         {
+            random_iterator = Random.Range(1, 4);
+            switch (random_iterator)
+            {
+                case 1:
+                    body_iterator = body_to_spawn_1;
+                    break;
+                case 2:
+                    body_iterator = body_to_spawn_2;
+                    break;
+                case 3:
+                    body_iterator = body_to_spawn_3;
+                    break;
+            }
+
+
             timer = 0.0f;
-            GameObject new_object = Instantiate(body_to_spawn, transform.position, Quaternion.identity);
+            GameObject new_object = Instantiate(body_iterator, transform.position, Quaternion.identity);
         }
     }
 }
