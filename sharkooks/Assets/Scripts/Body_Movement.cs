970fa602de121eb554465afd12b77169a2a4783e
orioldedios <orioldedios@gmail.com>
2024-06-27 13:28:15 +0200

    Head moving towards belt

---
+++ sharkooks/Assets/Scripts/Body_Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Body_Movement : MonoBehaviour
+{
+    public float speed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+    }
+}
