ede4da8f0af3b4321716118a7b2045aa4d6d5948
orioldedios <orioldedios@gmail.com>
2024-06-28 11:52:09 +0200

    sprite improvements

--- sharkooks/Assets/Scripts/Body_Movement.cs
+++ sharkooks/Assets/Scripts/Body_Movement.cs
@@ -8,13 +8,14 @@ public class Body_Movement : MonoBehaviour
     public float wrong_penalty = 0.8f;
     public GameObject bar_go = null;
     public GameObject game_manager = null;
-
+    //public Collider2D rubish_face_collider = null;
 
     // Start is called before the first frame update
     void Start()
     {
         bar_go = GameObject.Find("bar");
         game_manager = GameObject.Find("Game_Manager");
+        //rubish_face_collider = GameObject.Find("Rubbish_Face").GetComponent<Collider2D>();
         wrong_penalty = 0.2f;
     }
 
@@ -23,15 +24,25 @@ public class Body_Movement : MonoBehaviour
     {
         transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
 
-        if (transform.position.y < -6.0f)
-        {
-            bar_go.transform.localScale = new Vector3(bar_go.transform.localScale.x + wrong_penalty, bar_go.transform.localScale.y, bar_go.transform.localScale.z);
-            Destroy(this.gameObject);
-        }
+        //if (transform.position.y < -6.0f)
+        //{
+        //    bar_go.transform.localScale = new Vector3(bar_go.transform.localScale.x + wrong_penalty, bar_go.transform.localScale.y, bar_go.transform.localScale.z);
+        //    Destroy(this.gameObject);
+        //}
 
         
         if(speed > 0) speed = game_manager.GetComponent<difficulty_accel>().global_speed;
         else speed = game_manager.GetComponent<difficulty_accel>().global_speed * (-1);
 
     }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.name == "Rubbish" && collider.gameObject.transform.position.y < this.gameObject.transform.position.y)
+        {
+            bar_go.transform.localScale = new Vector3(bar_go.transform.localScale.x + wrong_penalty, bar_go.transform.localScale.y, bar_go.transform.localScale.z);
+            Destroy(this.gameObject);
+        }
+    }
+
 }
